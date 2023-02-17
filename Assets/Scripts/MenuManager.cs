f56302b100c12824bfce7d6d2bacc24d4a9385f0
NotTom31 <thomasmezzie@gmail.com>
2023-02-17 00:31:53 -0800

    Main menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject creditsMenu;
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void OpenCredits()
+    {
+        creditsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+}
