50e9d5028141ee0cbdd07c400241f91b14b8246e
torresta5 <torresta5@gmail.com>
2023-03-04 07:19:54 -0800

    Added comments/notes
    Added comments on the newly added lines of code on both PlayerController and HealthBar script to clarify what each line of code does.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,23 +6,23 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     //Slider for the Health Bar
-    public Slider slider;
+    public Slider healthSlider;
 
     //Sets the health of the player based on the integer parameter
     public void SetHealth(int health)
     {
         //Slider of the health bar will be set to the value of health
         //This will set the health in game
-        slider.value = health;
+        healthSlider.value = health;
     }
 
     //Sets the maximum value of the health to the slider based on the integer parameter
     public void SetMaxHealth(int health)
     {
         //Maximum value of the health bar slider is set
-        slider.maxValue = health;
+        healthSlider.maxValue = health;
 
         //The health bar slider will be set to that same maximum value
-        slider.value = health;
+        healthSlider.value = health;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,6 +43,7 @@ public class PlayerController : MonoBehaviour
         SetCountText();
         winTextObject.SetActive(false);
 
+        //Sets the current health to the max health
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
 
@@ -143,18 +144,24 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void TakeDamage(int damage)
+    //Reduces the player's health by the parameter value
+    //Function should be called whenever the player takes damage from an enemy
+    void PlayerDamage(int damage)
     {
+        //The player's health is subtracted by the damage value
         currentHealth = currentHealth - damage;
 
+        //The new player health that was subtracted is set
         healthBar.SetHealth(currentHealth);
     }
 
     void Update()
     {
+        //This is used as a test to ensure the health bar works properly
+        //Press the "V" key to reduce the player health
         if(Input.GetKeyDown(KeyCode.V))
         {
-            TakeDamage(2);
+            PlayerDamage(2);
         }
     }
 }
