d18e5885c51baf82006a386bd101b5b605d0007c
torresta5 <torresta5@gmail.com>
2023-03-17 13:01:52 -0700

    Created Test scene
    Alan's Test Scene.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
     void OnLook(InputValue inputValue)
     {
-        
+
     }
 
     void OnMove(InputValue movementValue)
@@ -88,7 +88,7 @@ public class PlayerController : MonoBehaviour
         //transform.forward = new Vector3(rb.velocity.x, 0, rb.velocity.z);
         transform.rotation = rotationResult;
 
-         // May be deprecated
+        // May be deprecated
         if (rb.velocity.sqrMagnitude > maxVelocity * maxVelocity) // Using sqrMagnitude for efficiency
         {
             rb.velocity = rb.velocity.normalized * maxVelocity;
@@ -175,42 +175,6 @@ public class PlayerController : MonoBehaviour
         //This is used as a test to ensure the health bar works properly
         //Press the "V" key to reduce the player health
         if (Input.GetKeyDown(KeyCode.V))
-        {
-            PlayerDamage(2);
-        }
-    }
-
-    //Reduces the player's health by the parameter value
-    //Function should be called whenever the player takes damage from an enemy
-    void PlayerDamage(int damage)
-    {
-        //The player's health is subtracted by the damage value
-        currentHealth = currentHealth - damage;
-
-        //The new player health that was subtracted is set
-        healthBar.SetHealth(currentHealth);
-
-        //When the player loses all health their position will be reset and the death count will increase by +1
-        //The player's health is reset back to it's max value
-        //The health slider is also reset to a full bar
-        if(currentHealth == 0)
-        {
-            transform.position = new Vector3(0f, 0.5f, 0f);
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            deathcount = deathcount + 1;
-            SetCountText();
-
-            currentHealth = maxHealth;
-            healthBar.SetMaxHealth(maxHealth);
-        }
-    }
-
-    void Update()
-    {
-        //This is used as a test to ensure the health bar works properly
-        //Press the "V" key to reduce the player health
-        if(Input.GetKeyDown(KeyCode.V))
         {
             PlayerDamage(2);
         }
