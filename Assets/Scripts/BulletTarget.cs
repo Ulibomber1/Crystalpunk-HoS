516a4f162f5047c00caedff01cf332da1c54410d
Daniel Ramirez <dramirtek99@gmail.com>
2023-04-26 11:39:07 -0700

    remove fog and add target identifier script

--- Assets/Scripts/BulletTarget.cs
+++ Assets/Scripts/BulletTarget.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//this script serves as an identifier,
+//paste it to whatever you want to serve as a target
+
 public class BulletTarget : MonoBehaviour
 {
     // Start is called before the first frame update
