f67669458a0c0f3630cb6a1f44504ed8ddbf6745
NotTom31 <thomasmezzie@gmail.com>
2023-05-10 15:23:07 -0700

    moving platform works

--- Assets/Scripts/oscillation.cs
+++ Assets/Scripts/oscillation.cs
@@ -8,23 +8,25 @@ public class oscillation : MonoBehaviour
     public float amp;
     public float freq;
     public float axis;
+    private Rigidbody rb;
     private void Start()
     {
         initPos = transform.position;
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         switch (axis)
         {
             case 0:
-                transform.position = new Vector3(Mathf.Sin(Time.time * freq) * amp + initPos.x, initPos.y, initPos.z);
+                rb.MovePosition(new Vector3(Mathf.Sin(Time.time * freq) * amp + initPos.x, initPos.y, initPos.z));
                 break;
             case 1:
-                transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, initPos.z);
+                rb.MovePosition(new Vector3(initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, initPos.z));
                 break;
             case 2:
-                transform.position = new Vector3(initPos.x, initPos.y, Mathf.Sin(Time.time * freq) * amp + initPos.z);
+                rb.MovePosition(new Vector3(initPos.x, initPos.y, Mathf.Sin(Time.time * freq) * amp + initPos.z));
                 break;
             default:
                 break;
