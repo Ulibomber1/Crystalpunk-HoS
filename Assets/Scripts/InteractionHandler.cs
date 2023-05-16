478667794879dae66fc70b251ebe705e392c8209
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-05-16 16:06:58 -0700

    Pass 1

--- Assets/Scripts/InteractionHandler.cs
+++ Assets/Scripts/InteractionHandler.cs
@@ -6,11 +6,13 @@ public class InteractionHandler : MonoBehaviour
 {
     [SerializeField] protected string togglerName;
     private MeshRenderer mr;
+    private MeshCollider mc;
     // Start is called before the first frame update
     protected virtual void Awake()
     {
         Interactable.OnInteractAction += OnInteractHandler;
         mr = gameObject.GetComponent<MeshRenderer>();
+        mc = gameObject.GetComponent<MeshCollider>();
     }
 
     protected virtual void OnDestroy()
@@ -22,10 +24,17 @@ public class InteractionHandler : MonoBehaviour
     {
         if (name != togglerName)
             return;
-        if(mr.enabled)
+        if (mr.enabled)
+        {
             mr.enabled = false;
+            mc.enabled = false;
+        }
         else
+        {
             mr.enabled = true;
+            mc.enabled = true;
+        }
+           
         
 
         Debug.Log("Object " + mr.enabled);
