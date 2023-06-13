01351423b78bbd499758ed7240fa42c08b89eab9
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2023-06-12 20:14:17 -0700

    Dialogue sound effects

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,7 +7,10 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance;
 
     [SerializeField] private AudioSource _musicSource, _effectSource;
-
+    [SerializeField] List<AudioClip> dialogueClips = new List<AudioClip>();
+    [SerializeField] AudioClip nextLine;
+    //[SerializeField] AudioClip menuNext;
+    //[SerializeField] AudioClip menuBack;
     private void Awake()
     {
         if (Instance == null)
@@ -26,5 +29,15 @@ public class SoundManager : MonoBehaviour
         _effectSource.PlayOneShot(clip);
     }
 
+    public void PlayDialogueSFX()
+    {
+        AudioClip clip = dialogueClips[Random.Range(0, dialogueClips.Count)];
 
+        _effectSource.PlayOneShot(clip, 0.3f);
+    }
+
+    public void PlayNextLine()
+    {
+        _effectSource.PlayOneShot(nextLine, 0.4f);
+    }
 }
